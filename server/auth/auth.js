b2349274c71dcc4ab56a00134f6c3b2c2ed8013d
Brayan-chan <al071392@uacam.mx>
2025-05-15 16:44:35 -0600

    feat: agregar archivo de autenticación con funciones de Firebase

---
+++ server/auth/auth.js
@@ -0,0 +1,10 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    sendPasswordResetEmail,
+    updateProfile,
+    setPersistence,
+    browserSessionPersistence
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
